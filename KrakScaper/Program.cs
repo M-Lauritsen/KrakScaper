7a7a37bca91c79f133963518e385f83021d1f66b
Michael Lauritsen <i30288741@live.dk>
2018-11-20 13:46:06 +0100

    added a try catch to prevent it from crashing if there was no result's

--- KrakScaper/Program.cs
+++ KrakScaper/Program.cs
@@ -24,57 +24,65 @@ namespace KrakScaper
             var url = "https://www.krak.dk/" + q +"/personer";
             Console.Clear();
 
-           
-            // Using the http client with Async
-            var httpClient = new HttpClient();
-            var html = await httpClient.GetStringAsync(url);
 
-            // Load the html document
-            var HtmlDocument = new HtmlDocument();
-            HtmlDocument.LoadHtml(html);
-
-            // a list of every result
-            var NameList = HtmlDocument.DocumentNode.Descendants("ul")
-                .Where(node => node.GetAttributeValue("class", "")
-                .Equals("ResultList")).ToList();
-
-            // Single person added to list
-            var NameListPeople = NameList[0].Descendants("div")
-                .Where(node => node.GetAttributeValue("class", "")
-                .Equals("PersonResultListItem")).ToList();
-
-            
-            // for each person in our NameListPeople
-            foreach (var person in NameListPeople)
+            try
             {
-                try
-                {
-                    //Names
-                    Console.WriteLine(person.Descendants("div")
-                    .Where(node => node.GetAttributeValue("class", "")
-                    .Equals("personName")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+                // Using the http client with Async
+                var httpClient = new HttpClient();
+                var html = await httpClient.GetStringAsync(url);
 
-                    // Address
-                    Console.WriteLine(person.Descendants("div")
-                    .Where(node => node.GetAttributeValue("class", "")
-                    .Equals("address")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+                // Load the html document
+                var HtmlDocument = new HtmlDocument();
+                HtmlDocument.LoadHtml(html);
 
-                    // PhoneNumber
-                    Console.WriteLine(person.Descendants("div")
-                    .Where(node => node.GetAttributeValue("role", "")
-                    .Equals("button")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t').Substring(0,11));
-                }
-                catch (Exception)
+                // a list of every result
+                var NameList = HtmlDocument.DocumentNode.Descendants("ul")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("ResultList")).ToList();
+
+                // Single person added to list
+                var NameListPeople = NameList[0].Descendants("div")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("PersonResultListItem")).ToList();
+
+                // for each person in our NameListPeople
+                foreach (var person in NameListPeople)
                 {
-                    Console.WriteLine("----Skjult----");
+                    try
+                    {
+                        //Names
+                        Console.WriteLine(person.Descendants("div")
+                        .Where(node => node.GetAttributeValue("class", "")
+                        .Equals("personName")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+
+                        // Address
+                        Console.WriteLine(person.Descendants("div")
+                        .Where(node => node.GetAttributeValue("class", "")
+                        .Equals("address")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t'));
+
+                        // PhoneNumber
+                        Console.WriteLine(person.Descendants("div")
+                        .Where(node => node.GetAttributeValue("role", "")
+                        .Equals("button")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t').Substring(0, 11));
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("----Skjult----");
+                        Console.WriteLine();
+                        continue;
+                    }
+
                     Console.WriteLine();
-                    continue;
                 }
-
-                
-
-                Console.WriteLine();
             }
+            catch (Exception)
+            {
+                // Hvis der ikke er nogle resultater
+                Console.WriteLine("ingen fundet");
+            }
+            
+            
+            
 
 
 
